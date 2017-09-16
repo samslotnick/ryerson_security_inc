530c3ee00f27203b3ed88a2690eb52675ea4d344
samslotnick <me@samslotnick.com>
2017-09-16 01:46:16 -0400

    gitignore added, gpslocation from google geo api

--- scrape.py
+++ scrape.py
@@ -1,6 +1,34 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from datetime import datetime
+import json
+import configparser
+config = configparser.ConfigParser()
+config.read("config.ini")
+api_key = config['KEYS']['api_key']
+#api_key='AIzaSyDMBNDabjlj7zqLLGjTyA-C7z0poNrHBLY'
+def getGeo(location, api_key):
+    streets = ["Alleyway","Alley","Sidewalk"]
+    location_ar = location.replace(",","").split(" ")
+    mod_location = ""
+    for word in location_ar:
+        if word.islower() != True and word.rstrip() not in streets or word.isdigit()== True:
+            mod_location += word + " "
+#    mod_location = location.lower()
+    mod_location = mod_location.replace(","," ").replace("sidewalk","").replace("near","").replace("alleyway","")
+    print(mod_location)
+    base = "https://maps.googleapis.com/maps/api/geocode/json?"
+    addL = "address=" + mod_location.replace(" ", "+") + "&key=" + api_key
+    geoURL = base + addL
+    response = urlopen(geoURL)
+    jsonRaw = response.read()
+    jsonData = json.loads(jsonRaw)
+    if jsonData['status'] == 'OK':
+        geoResult = jsonData['results'][0]['geometry']['location']
+    else:
+        geoResult = ""
+    return geoResult  
+
 page = 0
 links = []
 stats = []
@@ -21,22 +49,27 @@ while page < 50:
         soup2 = BeautifulSoup(sec_page, 'html.parser')
         paras = soup2.find_all("p")
         if paras[7].text != "LOCATION OF INCIDENT":
-            location = paras[7].text
+            location = paras[7].text.rstrip()
         else:    
-            location = paras[8].text
+            location = paras[8].text.rstrip()
         if paras[5].text != "DATE AND TIME OF INCIDENT":
-            date = paras[5].text
+            date = paras[5].text.rstrip()
         else:
-            date = paras[6].text
+            date = paras[6].text.rstrip()
         nature = soup2.find_all("h1")
         nature = nature[2].text.rstrip()
-        stats.append({"nature": nature, "date": date, "location":location})
+#        getGeo(location, api_key)
+        stats.append({"nature": nature, "date": date, "location":location, "geoLoc": getGeo(location, api_key)})
     page += 10
 print("----- %s seconds ----" % (time.time() - start_time))
-
+#print(stats)
+#place = google_maps.search(location=paras[8].text)
+#print(place)
 
 
 
 #==============================================================================
 # descrip_info    attempted    weapon knife rock arrest made  
 #==============================================================================
+
+
