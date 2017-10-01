6cfa042f6ff11b62b32fdb5125cd10c308843577
samslotnick <me@samslotnick.com>
2017-09-30 21:32:17 -0400

    converted to geojson

--- scrape.py
+++ scrape.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from datetime import datetime
 import json
+from geojson import Feature, Point, FeatureCollection
 import configparser
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -23,9 +24,11 @@ def getGeo(location, api_key):
     jsonRaw = response.read()
     jsonData = json.loads(jsonRaw)
     if jsonData['status'] == 'OK':
-        geoResult = jsonData['results'][0]['geometry']['location']
+        geoResult = []
+        geoResult.append(jsonData['results'][0]['geometry']['location']['lng'])
+        geoResult.append(jsonData['results'][0]['geometry']['location']['lat'])
     else:
-        geoResult = ""
+        geoResult = []
     return geoResult  
 
 def arrestFormat(nature):
@@ -37,7 +40,7 @@ def arrestFormat(nature):
 
 page = 0
 links = []
-stats = []
+collection = {"type":"FeatureCollection","features":[]}
 import time
 start_time = time.time()
 while page < 50:
@@ -77,16 +80,18 @@ while page < 50:
 #        getGeo(location, api_key)
         if "arrest made" in nature.lower():
             print(nature)
-        stats.append({"nature": nature, "date": date, "location":location, "geoLoc": getGeo(location, api_key), "weapon": weapon, "arrest": arrest})
+        stats = {"type":"Feature","properties":{"nature": nature,"date": date,"location":location,"weapon": weapon,"arrest": arrest},"geometry":{"type": "Point","coordinates":getGeo(location, api_key)}}
+        if len(stats["geometry"]["coordinates"]) > 0:
+            final = json.dumps(stats)
+#        print(stats)
+        outfile = open("data.json","a",encoding='utf-8')
+        outfile.write("\n"+final+",")
     page += 10
-outfile = open("data.txt","w",encoding='utf-8')
-outfile.write(json.dumps(stats))
 print("----- %s seconds ----" % (time.time() - start_time))
 outfile.close()
 #print(stats)
 #place = google_maps.search(location=paras[8].text)
 #print(place)
-
 
 
 
